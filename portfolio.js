564078fd36a88289ac66833c4b7ef8849a9423d6
joshuarigha <40634006+joshuarigha@users.noreply.github.com>
2018-06-27 06:36:45 -0700

    my first website
    my portfolio

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var j=document.getElementById("j")
+j.addEventListener("click",function(){var
+name=prompt("what is your name?");
+                                      alert("mambo vipi "+name);
+                                      alert("sawa+sawa");});
+// methods and function
